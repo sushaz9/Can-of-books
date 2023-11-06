2b50f0df537f78df715d0a4b954d2c0b24167c9b
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-06 13:53:37 +0000

    init

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+const app = express()
+app.use(cors())
+const PORT = 8080;
+const mongoose = require("mongoose")
+
+app.get("/", (_, response) => response.json("You are looking at my root route")
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`);
